a418ba562452d0488805c9838357ada255256fd9
liyiruo <401213245@qq.com>
2020-08-09 18:47:29 +0800

    设置了新的登录页面，和登录成功后进入的界面

--- liyiruo-security-core/src/main/java/security/controller/CustomLoginController.java
+++ liyiruo-security-core/src/main/java/security/controller/CustomLoginController.java
@@ -1,21 +1,12 @@
-package web.controller;
+package security.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class MainController {
-
-    @RequestMapping({"/","","index"})
-    public String index() {
-        return "index";
-    }
-
-
-
+public class CustomLoginController {
     @RequestMapping("/login/page")
     public String toLogin() {
         return "login";
     }
-
 }
--- liyiruo-security-web/src/main/java/web/security/config/SpringSecurityConfig.java
+++ liyiruo-security-web/src/main/java/web/security/config/SpringSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -24,13 +25,14 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
      * 认证管理器:
      * 1、认证信息提供方式(用户名、密码、当前用户的资源权限)
      * 2、可采用内存存储方式，也可能采用数据库方式等
+     *
      * @param auth
      * @throws Exception
      */
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         String password = passwordEncoder().encode("123");
-        log.info("加密后的密码=====>{}",password);
+        log.info("加密后的密码=====>{}", password);
         auth
                 .inMemoryAuthentication()
                 .withUser("admin")
@@ -45,15 +47,45 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
      * 3、定义认证方式:httpBasic 、httpForm
      * 4、定制登录页面、登录请求地址、错误处理方式
      * * 5、自定义 spring security 过滤器
+     *
      * @param http
      * @throws Exception
      */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic()
+        /**
+         * 使用form表单登录，
+         * 设置表单所在的页面 loginPage
+         * 设置页面的上用户名和密码 name 属性  usernameParameter  passwordParameter
+         * 设置登录后的进入的页面 loginProcessingUrl
+         */
+
+        //httpBasic 浏览器弹出一个认证框的方式认证
+//        http.httpBasic()
+        //formLogin 表单认证
+        http.formLogin()
+                .loginPage("/login/page")//指定登录页面 URL 需要在controller里
+                .loginProcessingUrl("/login/form")//认证成功后进入的页面 默认是/login
+                .usernameParameter("name")// 默认用户名的属性名是 username
+                .passwordParameter("pwd")// 默认密码的属性名是 password
                 .and()
                 .authorizeRequests()//认证请求
+                .antMatchers("/login/page")
+                .permitAll()
                 .anyRequest()
                 .authenticated();//所有进入应用的http请求都要进行认证
     }
+
+
+    /**
+     * 释放静态资源
+     *
+     * @param web
+     */
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers("/dist/**", "/modules/**", "/plugins/**");
+    }
+
+
 }

0dbe1453fab7846f97b90690bfeb97d49b2c4d6d
liyiruo <401213245@qq.com>
2020-09-07 22:49:10 +0800

    配置左侧菜单路径

--- liyiruo-security-web/src/main/java/web/controller/SysUserController.java
+++ liyiruo-security-web/src/main/java/web/controller/SysUserController.java
@@ -1,11 +1,14 @@
 package web.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * 创建用户管理控制层
  * @author liyiruo
  */
+@Controller
+@RequestMapping(value = "/user")
 public class SysUserController {
     private static final String HTML_PREFIX = "system/user/";
 
